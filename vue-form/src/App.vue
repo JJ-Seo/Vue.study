28343161281d13afadddf5fae649d2338ed55e66
Seo <jeongjae8087@gmail.com>
2022-01-12 13:58:40 +0900

    vue model 속성과 subnit event 처리

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <form action="">
+    <form v-on:submit="subnitForm">
       <div>
         <label for="username">id: </label>
-        <input type="text" id="username">
+        <input type="text" id="username" v-model="username">
       </div>
       <div>
         <label for="password">pw: </label>
-        <input type="password" id="password">
+        <input type="password" id="password" v-model="password">
       </div>
-      <button>Login</button>
+      <button type="submit">Login</button>
     </form>
 
   </div>
@@ -17,7 +17,18 @@
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    subnitForm: function(event){
+      event.preventDefault();
+      console.log(this.username, this.password);
+    }
+  }
 }
 </script>
 
