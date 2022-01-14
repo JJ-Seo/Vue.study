eb51626e9b3459edca6a35a2f1013f0de505deff
Seo <jeongjae8087@gmail.com>
2022-01-14 16:18:58 +0900

    할일, 완료, 전체 버튼 생성

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -5,11 +5,18 @@
         <h1 class="text-center mb-4">Todo App</h1>
         <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
 
-        <div class="list-group">
+        <div class="list-group mb-4">
           <button class="list-group-item text-left" v-for="todo in activeTodoList" :key="todo" @click="toggleTodoState(todo)">
             {{ todo.label }}
           </button>
         </div>
+
+        <div class="text-right">
+          <button type="button" class="btn btn-sm">To do List</button>
+          <button type="button" class="btn btn-sm">Done</button>
+          <button type="button" class="btn btn-sm">All</button>
+        </div>
+
       </div>
     </div>
   </div>
